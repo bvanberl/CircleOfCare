970dbfc5d37ff0c9ec488a882f2095a7da33beb4
bvanberl <bvanberl@uwo.ca>
2018-03-27 18:48:19 -0400

    Added more front-end frameworks, controllers for user creation, login, profile view

---
+++ public/js/services/AuthenticationService.js
@@ -0,0 +1,82 @@
+(function () {
+
+  angular
+    .module('circleofcare')
+    .service('authentication', authentication);
+
+  authentication.$inject = ['$http', '$window'];
+  function authentication ($http, $window) {
+
+    var saveToken = function (token) {
+      $window.localStorage['circleofcare-token'] = token;
+    };
+
+    var getToken = function () {
+      return $window.localStorage['circleofcare-token'];
+    };
+
+    var isLoggedIn = function() {
+      var token = getToken();
+      var payload;
+
+      if(token){
+        payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+
+        return payload.exp > Date.now() / 1000;
+      } else {
+        return false;
+      }
+    };
+
+    var currentUser = function() {
+      if(isLoggedIn()){
+        var token = getToken();
+        var payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+        return {
+          email : payload.email,
+          name : payload.name
+        };
+      }
+    };
+
+    register = function(user) {
+      return $http.post('/api/register', user)
+      .then(function(res){
+        if(res.status === 200){
+          saveToken(res.data.token);
+        }
+      });
+    };
+
+    login = function(user) {
+      return $http.post('/api/login', user).then(function(res) {
+        if(res.status === 200){
+          saveToken(res.data.token);
+        }
+        else{
+          console.log('Incorrect credentials!');
+        }
+      });
+    };
+
+    logout = function() {
+      $window.localStorage.removeItem('circleofcare-token');
+    };
+
+    return {
+      currentUser : currentUser,
+      saveToken : saveToken,
+      getToken : getToken,
+      isLoggedIn : isLoggedIn,
+      register : register,
+      login : login,
+      logout : logout
+    };
+  }
+
+
+})();
